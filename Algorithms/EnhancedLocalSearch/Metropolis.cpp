04b5c5736970d9f628377b813912834f34538e27
mazur <mazur@Personal>
2015-11-17 20:57:22 +0100

    bug fixture

--- Algorithms/EnhancedLocalSearch/Metropolis.cpp
+++ Algorithms/EnhancedLocalSearch/Metropolis.cpp
@@ -32,15 +32,14 @@ void CMetropolis::performWithin()
 	std::random_device rd;
     std::mt19937 randomGen(rd());
     std::uniform_int_distribution<int> intDist(0, m_distance.size()-1);
-    std::uniform_real_distribution<double> realDist(0.0, 1.0);
-    //auto maxGradient = std::log(m_alfa) * m_temperature * m_temperature; 
-	
+    std::uniform_real_distribution<double> realDist(0.0, 0.999);
+
 	do
 	{
 		currentTemp = std::pow(m_alfa, K) * m_temperature;
 		++m_steps;
-		//unsigned L = std::log( maxGradient / ( currentTemp * std::pow(m_alfa, K) * std::log(m_alfa) * m_temperature) );
-		for(size_t idx = 0; idx<m_distance.size()*m_distance.size()/2; ++idx)
+		unsigned L = K+1;
+		for(size_t idx = 0; idx<L; ++idx)
 		{
 			int i = 0;
 			int j = 0;
@@ -55,21 +54,30 @@ void CMetropolis::performWithin()
 			computeCost(swapedIdxs);
 			++m_reviewedSolutions;
 
-			auto accept = std::exp(-(m_cost - currentCost)/currentTemp);
-			if(accept > realDist(randomGen))
+			if(m_cost < currentCost)
 			{
 				std::swap(m_result[i], m_result[j]);
 				currentCost = m_cost;
 				adaptiveTemp = currentTemp;
-			} 
-			else
+			}
+			else 
 			{
-				m_cost = currentCost;
+				auto accept = std::exp(-(m_cost - currentCost)/currentTemp);
+
+				if(accept > realDist(randomGen))
+				{
+					std::swap(m_result[i], m_result[j]);
+					currentCost = m_cost;
+				} 
+				else
+				{
+					m_cost = currentCost;
+				}
 			}
 		}
 
 		++K;
 	}
-	while(currentTemp > 1);
+	while(currentTemp > 0.5);
 	m_temperature = adaptiveTemp;
 }
